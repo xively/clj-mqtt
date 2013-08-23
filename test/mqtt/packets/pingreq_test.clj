8c83fccc7939324c591dac69ff1cd2d3d7381cfe
Paul Bellamy <paul.a.bellamy@gmail.com>
2013-08-23 12:55:10 +0000

    adding pingreq

--- test/mqtt/packets/pingreq_test.clj
+++ test/mqtt/packets/pingreq_test.clj
@@ -1,7 +1,29 @@
 (ns mqtt.packets.pingreq-test
   (:use clojure.test
         mqtt.test_helpers
-        mqtt.decoder))
+        mqtt.decoder
+        mqtt.encoder
+        mqtt.packets.common
+        mqtt.packets.pingreq)
+  (:import [io.netty.buffer Unpooled]))
+
+(deftest pingreq-validate-message-test
+  (testing "returns when valid"
+    (let [packet {:type :pingreq}]
+      (is (= packet (validate-message packet))))))
+
+(deftest encoding-pingreq-packet-test
+  (testing "when encoding a simple pingreq packet"
+    (let [encoder (make-encoder)
+          packet  {:type :pingreq}
+          out     (Unpooled/buffer 2)
+          _       (.encode encoder nil packet out)]
+      (is (= (byte-buffer-to-bytes out) 
+             (into [] (bytes-to-byte-array
+                        ;; fixed header
+                        0xC0
+                        ;; remaining length
+                        0x00)))))))
 
 (deftest decoding-pingreq-packet-test
   (testing "when parsing a simple Pingreq packet"

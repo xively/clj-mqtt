19f0574478307b88ee53c9a3af1d9a0e28f24a81
Paul Bellamy <paul.a.bellamy@gmail.com>
2013-08-23 12:51:41 +0000

    adding disconnect packet encoding

--- test/mqtt/packets/disconnect_test.clj
+++ test/mqtt/packets/disconnect_test.clj
@@ -1,7 +1,29 @@
 (ns mqtt.packets.disconnect-test
   (:use clojure.test
         mqtt.test_helpers
-        mqtt.decoder))
+        mqtt.decoder
+        mqtt.encoder
+        mqtt.packets.common
+        mqtt.packets.disconnect)
+  (:import [io.netty.buffer Unpooled]))
+
+(deftest disconnect-validate-message-test
+  (testing "returns nil when valid"
+    (let [packet {:type :disconnect}]
+      (is (= packet (validate-message packet))))))
+
+(deftest encoding-disconnect-packet-test
+  (testing "when encoding a simple disconnect packet"
+    (let [encoder (make-encoder)
+          packet  {:type :disconnect}
+          out     (Unpooled/buffer 2)
+          _       (.encode encoder nil packet out)]
+      (is (= (byte-buffer-to-bytes out) 
+             (into [] (bytes-to-byte-array
+                        ;; fixed header
+                        0xE0
+                        ;; remaining length
+                        0x00)))))))
 
 (deftest decoding-disconnect-packet-test
   (testing "when parsing a simple disconnect packet"
